4e462c45865c3fc2dae25b67212990d2783449e0
Gabriela <gabrielaog97@gmail.com>
2022-05-15 14:34:41 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# Imagens negativas com Sucuri 
+[![Python 3.8.10](https://img.shields.io/badge/python-3.8.10-blue.svg)](https://www.python.org/downloads/release/python-360/)
+
+### Trabalho realizado por: 
+- Caio Saud
+- Carolina Carvalhosa
+- Gabriela Gonçalves
+
+## Descrição
+
+Utilizamos a biblioteca Sucuri para transformar imagens coloridas em imagens negativas para a matéria de Sistemas Distribuídos.
+
+
+## Instalação
+
+1. Instalar a [Python Imaging Library](https://pillow.readthedocs.io/en/stable/installation.html) através dos comandos:
+
+    ```
+    python3 -m pip install --upgrade pip
+    python3 -m pip install --upgrade Pillow
+    ```
+
+2. Instalar as dependências necessárias para rodar o projeto:
+
+    ```
+    pip install numpy scikit-image
+    ```
+
+
+## Rodando o Projeto
+
+Já existem imagens dentro da pasta `imgs`, mas, caso queira, é possível adicionar novas.
+
+Para rodar o projeto, execute o comando abaixo.
+
+```
+python3 main.py 1
+```
+
+Observe que a pasta [negative_images](./negative_imgs/) está inicialmente vazia, pois o processo para torná-las negativas ainda não foi iniciado. Após rodar o comando, as imagens negativas serão inseridas automaticamente nessa pasta. 
+
+> O argumento passado é o número de workers. No caso do exemplo, temos apenas 1.
+
+> Testado em distribuição Debian GNU/Linux 
+
+
+## Resultados
+
+Segue abaixo um exemplo do resultado obtido.
+
+<div style="display:flex; align:center; text-align:center">
+    <div>
+        <strong>Antes</strong>
+        <img src="./results/original_image.jpeg" width="350" alt="Antes">
+    </div>
+    <div>
+        <strong>Depois</strong>
+        <img src="./results/negative_image.png" width="350" alt="Depois">
+    </div>
+</div>
---
+++ main.py
@@ -0,0 +1,80 @@
+import os
+import sys
+import time
+import numpy as np
+import skimage
+from skimage import io
+from PIL import Image
+import pathlib
+from pyDF import *
+
+save_dir = 'negative_imgs'
+imgs_folder = str(os.path.dirname(os.path.realpath(__file__))) + "/imgs"
+
+def list_imgs(rootdir):
+    """ Lista os nomes dos arquivos de imagens """
+    file_names = []
+    for current, directories, files in os.walk(rootdir):
+        for f in files:
+            file_names.append(current + '/' + f)
+
+    file_names.sort()   
+    return file_names
+
+def get_negative(args):
+    """ Converte a imagem no seu negativo """
+    fname = args[0]
+    splitname = fname.split('/')[-1]
+
+    img = io.imread(fname)
+    
+    red = img[:,:,0]
+    green = img[:,:,1]
+    blue = img[:,:,2]
+
+    img_ch0_R_neg = 255-red
+    img_ch1_G_neg = 255-green
+    img_ch2_B_neg = 255-blue
+
+    new_image = np.stack((img_ch0_R_neg, img_ch1_G_neg, img_ch2_B_neg), axis=-1)
+
+    splitname = save_dir + '/' + 'negative_' + splitname.split('.')[0]  + '.png'
+    Image.fromarray(new_image).convert("RGB").save(splitname)
+
+    return splitname
+
+
+def print_name(args):
+    """ Formata e imprime os nomes """
+    fname = args[0]
+
+    print("Convertido %s" %fname)
+
+def sucuri(n_procs):
+    """ Aplica comandos da lib Sucuri """
+    nprocs = n_procs
+    imagePath = list_imgs(imgs_folder)
+    
+    graph = DFGraph()
+    sched = Scheduler(graph, nprocs, mpi_enabled = False)
+    
+    feed_files = Source(imagePath)
+    
+    convert_file = FilterTagged(get_negative, 1)  
+    
+    pname = Serializer(print_name, 1)
+    
+    graph.add(feed_files)
+    graph.add(convert_file)
+    graph.add(pname)
+
+    feed_files.add_edge(convert_file, 0)
+    convert_file.add_edge(pname, 0)
+
+    t0 = time.time()
+    sched.start()
+    t1 = time.time()
+    print( "Tempo de execucao: %.3f" %(t1-t0))
+
+
+sucuri(int(sys.argv[1]))
